ff3bb3681c40bd054854dddf73c509c9b3e4fbc3
DC <jessy.charlot@outlook.fr>
2021-04-10 20:16:01 +0200

    Add routes and manage errors

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,10 +1,47 @@
 const express = require('express')
 
+const HttpError = require('../models/http-error')
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    console.log('GET Request in places');
-    res.json({message: 'It works'})
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the World!',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878531
+        },
+        address: '20 W 34th St, New York, NY 10001',
+        creator: 'u1'
+    }
+]
+
+router.get('/:pid', (req, res, next) => {
+    const placeId = req.params.pid;
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placeId
+    })
+
+    if(!place) {
+        throw new HttpError('Could not find a place with the provided id', 404)
+    }
+
+    res.json({place});
+})
+
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid;
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId
+    })
+
+    if(!place) {
+        return next(new HttpError('Could not find a place with the provided user id', 404));
+    }
+
+    res.json({place});
 })
 
 module.exports = router;
