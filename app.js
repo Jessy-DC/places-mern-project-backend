7996b6284920e309d31e5e30b28e0220ddcb208f
DC <jessy.charlot@outlook.fr>
2021-04-21 21:43:00 +0200

    Fix CORS error

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ const {ACCESS_MONGO} = require('./key/key')
 
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+    next();
+})
+
 const HttpError = require('./models/http-error')
 
 app.use('/api/places', placesRoutes);
@@ -30,7 +41,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-    .connect(`mongodb+srv://Jessy:${ACCESS_MONGO}@cluster0.lh6or.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+    .connect(`mongodb+srv://Jessy:${ACCESS_MONGO}@cluster0.lh6or.mongodb.net/mern?retryWrites=true&w=majority`)
     .then(() => {
         app.listen(5000, () => {
             console.log('Listening on port 5000')
