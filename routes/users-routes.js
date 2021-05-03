4c0e6c27ab388d1a2f2a2ba32c6f6b73c16e66c5
Jessy CHARLOT <50245217+Jessy-DC@users.noreply.github.com>
2021-05-03 15:19:00 +0200

    Prepare backend for upload file

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,22 +1,25 @@
-const express = require('express')
-const { check } = require('express-validator')
+const express = require('express');
+const { check } = require('express-validator');
 
-const usersController = require('../controller/users-controller')
+const usersController = require('../controller/users-controller');
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
 router.get('/', usersController.getUsers);
 
-router.post('/signup',[
-    check('name')
-        .not()
-        .isEmpty(),
-    check('email')
-        .normalizeEmail()
-        .isEmail().withMessage('Email must be valid'),
-    check('password')
-        .isLength({min: 7})
-], usersController.createUser);
+router.post('/signup',
+    fileUpload.single('image'),
+    [
+        check('name')
+            .not()
+            .isEmpty(),
+        check('email')
+            .normalizeEmail()
+            .isEmail().withMessage('Email must be valid'),
+        check('password')
+            .isLength({min: 7})
+    ], usersController.createUser);
 
 router.post('/login', usersController.logUserIn)
 
