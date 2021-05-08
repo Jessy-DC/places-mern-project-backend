5a608f7bffe52572f30c34d39e95cb422d943b4f
Jessy CHARLOT <50245217+Jessy-DC@users.noreply.github.com>
2021-05-08 18:56:31 +0200

    Use config file for key

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-    .connect(`mongodb+srv://Jessy:${ACCESS_MONGO}@cluster0.lh6or.mongodb.net/mern?retryWrites=true&w=majority`)
+    .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lh6or.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
     .then(() => {
         app.listen(5000, () => {
             console.log('Listening on port 5000')
